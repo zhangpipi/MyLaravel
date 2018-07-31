75c3fea346b2856eb22fec23c1854c4d6ac920cb
zhangbaiyu <zhangbaiyu@gmail.com>
2018-07-31 14:51:03 +0800

    完成用户注册

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,12 @@ class User extends Authenticatable
 
     protected $table = 'users';
 
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
+
     /**
      * The attributes that are mass assignable.
      *
