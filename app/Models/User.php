d36d2652590085cbb6fc9666cc8dd3da944c1271
zhangbaiyu <zhangbaiyu@gmail.com>
2018-08-01 22:12:03 +0800

    完成微博功能

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,17 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
+    public function feed()
+    {
+        return $this->statuses()
+            ->orderBy('created_at', 'desc');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -9,6 +9,16 @@
                         @include('shared._user_info', ['user' => $user])
                     </section>
                 </div>
+                <div class="col-md-12">
+                    @if (count($statuses) > 0)
+                        <ol class="statuses">
+                            @foreach ($statuses as $status)
+                                @include('statuses._status')
+                            @endforeach
+                        </ol>
+                        {!! $statuses->render() !!}
+                    @endif
+                </div>
             </div>
         </div>
     </div>
