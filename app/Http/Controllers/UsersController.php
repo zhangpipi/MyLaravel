9ec0f299b6b850a3f7e18021371cbcd2a71c78ee
zhangbaiyu <zhangbaiyu@gmail.com>
2018-08-01 14:22:09 +0800

    添加账户激活功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,29 +6,32 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Models\User;
 use Auth;
+use Mail;
 
 class UsersController extends Controller
 {
-    public function __construct()
-    {
+    public function __construct(){
         $this->middleware('auth', [
-            'except' => ['show', 'create', 'store', 'index']
+            'except' => ['show', 'create', 'store', 'index', 'confirmEmail']
         ]);
         $this->middleware('guest', [
             'only' => ['create']
         ]);
     }
-    public function index()
-    {
+
+    public function index(){
         $users = User::paginate(10);
         return view('users.index', compact('users'));
     }
+
     public function create(){
         return view('users.create');
     }
+
     public function show(User $user){
         return view('users.show',compact('user'));
     }
+
     public function store(Request $request){
         $this->validate($request, [
             'name' => 'required|max:50',
@@ -42,14 +45,16 @@ class UsersController extends Controller
             'password' => bcrypt($request->password),
         ]);
 
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+        return redirect('/');
     }
+
     public function edit(User $user){
         $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
+
     public function update(User $user, Request $request){
         $this->validate($request, [
             'name' => 'required|max:50',
@@ -65,10 +70,36 @@ class UsersController extends Controller
         session()->flash('success', '个人资料更新成功！');
         return redirect()->route('users.show', $user->id);
     }
+
     public function destroy(User $user)
     {
         $user->delete();
         session()->flash('success', '成功删除用户！');
         return back();
+    }
+
+    public function confirmEmail($token){
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    protected function sendEmailConfirmationTo($user){
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'aufree@yousails.com';
+        $name = 'Aufree';
+        $to = $user->email;
+        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+
+        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,14 @@ class User extends Authenticatable
 
     protected $table = 'users';
 
+    public static function boot(){
+        parent::boot();
+
+        static::creating(function ($user) {
+            $user->activation_token = str_random(30);
+        });
+    }
+
     public function gravatar($size = '100')
     {
         $hash = md5(strtolower(trim($this->attributes['email'])));
